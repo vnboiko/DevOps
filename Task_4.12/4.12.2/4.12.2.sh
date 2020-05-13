8e87e9bcfe84c45820d505a642741b11f43e7c44
Vladimir Boiko <vnboyko.work@gmail.com>
2020-05-13 17:40:15 +0300

    ReAdd Task 4.12

--- Task_4.12/4.12.2/4.12.2.sh
+++ Task_4.12/4.12.2/4.12.2.sh
@@ -3,21 +3,22 @@ while :
 do
 if [ "$var1" == "0" ]
 then
-md5sum -c checksum.md5
-var1="$?"
-echo "var1= "$var1
 sleep 5
+md5sum -c /home/jamesbond/Desktop/Epam/4.12/checksum.md5
+var1="$?"
+md5sum * */* 2>/dev/null > /home/jamesbond/Desktop/Epam/4.12/checksum.md5
+echo "var1= "$var1
 else
 #архивация проекта
-tar -zcf /home/jamesbond/Desktop/Epam/4.12/4.12.2/arc_$(date +%d%m%y,%T).tgz /home/jamesbond/Desktop/Epam/4.12/4.12.2
+tar -zcf /home/jamesbond/Desktop/Epam/4.12/arc/arc_$(date +%d%m%y,%T).tgz /home/jamesbond/Desktop/Epam/4.12/4.12.2
 #запустить перекомпиляцию проекта
 make -f Makefile-2 clean
-#запись контрольной суммы в файл
-md5sum factorial.cpp functions.h hello.cpp main.cpp > checksum.md5
+#вычисление контрольных сумм проекта и запись в файл
+md5sum * */* 2>/dev/null > /home/jamesbond/Desktop/Epam/4.12/checksum.md5
 sleep 5 
-#проверить не изменилась ли контрольная сумма
-md5sum -c checksum.md5
-#0-если не изменилась, 1-если иеменилась, записали значение в var1
+#проверить не изменились ли контрольные суммы
+md5sum -c /home/jamesbond/Desktop/Epam/4.12/checksum.md5
+#0-если не изменились, 1-если иеменились, записали значение в var1
 var1="$?"
 echo "var1= "$var1
 fi
